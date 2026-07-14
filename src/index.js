99da61982b88f745b49ebcfd770bbaaca597cf31
laukiksoradis13 <laukik.p@soradisdidital.com>
2026-07-14 13:08:09 +0530

    Text Change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello AWS ! This is my new instance of EC2 server with CI/CD pipeline and auto deployment using GitHub Actions.");
+  res.send("Hello AWS !. This is my new instance of EC2 server with CI/CD pipeline and auto deployment using GitHub Actions.");
 });
 
 app.listen(5000);
